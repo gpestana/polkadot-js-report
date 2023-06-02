f71703c2d48f06e433ac754ff2bba025d55b3e45
Ankan <ankan.anurag@gmail.com>
2023-06-02 10:40:22 +0200

    minor refactor

--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,54 @@
 import '@polkadot/api-augment';
 import '@polkadot/types-augment';
 
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { Balance } from '@polkadot/types/interfaces/runtime';
+import {ApiPromise, WsProvider} from '@polkadot/api';
+import {Balance} from '@polkadot/types/interfaces/runtime';
 import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { BN } from '@polkadot/util';
+import {hideBin} from 'yargs/helpers';
 
 const optionsPromise = yargs(hideBin(process.argv)).option('endpoint', {
-	alias: 'e',
-	type: 'string',
-	default: 'wss://rpc.polkadot.io',
-	description: 'the wss endpoint. It must allow unsafe RPCs.',
-	required: true
+    alias: 'e',
+    type: 'string',
+    default: 'wss://kusama-rpc.polkadot.io',
+    description: 'the wss endpoint. It must allow unsafe RPCs.',
+    required: true
 }).argv;
 
 async function main() {
-	const options = await optionsPromise;
-	const provider = new WsProvider(options.endpoint);
-	const api = await ApiPromise.create({ provider });
+    const options = await optionsPromise;
+    const provider = new WsProvider(options.endpoint);
+    const api = await ApiPromise.create({provider});
 
-	console.log(
-		`Connected to node: ${options.endpoint} ${(await api.rpc.system.chain()).toHuman()} [ss58: ${
-			api.registry.chainSS58
-		}]`
-	);
+    console.log(
+        `****************** Connected to node: ${options.endpoint} ${(await api.rpc.system.chain()).toHuman()} [ss58: ${
+            api.registry.chainSS58
+        }] ******************`
+    );
 
-	for (const key in api.query) {
-		if (api.query[key] && api.query[key].palletVersion) {
-			console.log(key, (await api.query[key].palletVersion()).toHuman());
-		}
-	}
+    // await which_pallets(api);
+    // await read_const(api);
+	await subscribe_finalized_blocks(api);
 
-	// reading a constant
-	const ED: Balance = api.consts.balances.existentialDeposit;
-	console.log(ED.toHuman());
 
-	// subscribe to finalized blocks:
+}
+
+async function subscribe_finalized_blocks(api: ApiPromise) {
 	const unsub = await api.rpc.chain.subscribeFinalizedHeads((header) => {
 		console.log(`finalized block #${header.number}`);
 	});
+}
+async function which_pallets(api: ApiPromise) {
+    console.log(`Found following pallets with their version`);
+    for (const key in api.query) {
+        if (api.query[key] && api.query[key].palletVersion) {
+            console.log(key, (await api.query[key].palletVersion()).toHuman());
+        }
+    }
+}
+
+async function read_const(api: ApiPromise) {
+    const ED: Balance = api.consts.balances.existentialDeposit;
+    console.log(`Existential deposit: ${ED.toHuman()}`);
 }
 
 main().catch(console.error);
